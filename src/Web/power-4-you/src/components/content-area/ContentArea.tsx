7209449f108149faa7212019872d24b00a058afe
trujon2809 <trujon2809@sus.gbs-grafschaft.de>
2026-05-17 21:01:37 +0200

    Add TODO

--- src/Web/power-4-you/src/components/content-area/ContentArea.tsx
+++ src/Web/power-4-you/src/components/content-area/ContentArea.tsx
@@ -10,6 +10,8 @@ const ContentArea = (
         setSelectedOverview: unknown 
     }
 ) => {
+    //TODO: Tabelle mit Solarmodulen pro Kunde, mit Detailbereich für den Graphen? Was noch?
+
     return (
         <div className="flex-row content-area margin-bottom-large border-radius">
             {selectedOverview}
