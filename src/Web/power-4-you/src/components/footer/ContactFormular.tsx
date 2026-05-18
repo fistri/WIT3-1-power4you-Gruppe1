6781d0db45b6ab516125b055fdad3f6986cfb3c8
Knaap Arendvander <knaare0102@sus.gbs-grafschaft.de>
2026-05-18 21:18:31 +0200

    Added Imprint and prepared Contact Formular

---
+++ src/Web/power-4-you/src/components/footer/ContactFormular.tsx
@@ -0,0 +1,7 @@
+import { memo } from "react"
+
+const ContactFormular = () => {
+    return (<div>test</div>)
+}
+
+export default memo(ContactFormular)
--- src/Web/power-4-you/src/components/footer/Footer.tsx
+++ src/Web/power-4-you/src/components/footer/Footer.tsx
@@ -1,37 +1,41 @@
 import { Button, Modal } from "rsuite"
 import "./footer.css"
 import { memo, useState } from "react"
+import { Drawer } from 'rsuite'
+import ContactFormular from "./ContactFormular"
 
 const Footer = () => {
     const [open, setOpen] = useState(false)
+    const [currentlyOpen, setCurrentlyOpen] = useState<"Imprint"|"Contact"|"">("")
 
-    const handleOpen = () => setOpen(true)
-    const handleClose = () => setOpen(false)
+    const handleOpenClick = (event : any) => {
+        setOpen(true)
+        setCurrentlyOpen(event.target.innerText)
+    }
 
     return (
         <div className="flex-row border-radius centered footer">
-            <Button appearance="subtle" className="margin-right-small">
+            <Button appearance="subtle" className="margin-right-small" onClick={handleOpenClick}>
                 Imprint
             </Button>
-            <Button appearance="subtle" onClick={handleOpen}>
+            <Button appearance="subtle" onClick={handleOpenClick}>
                 Contact
             </Button>
-            <Modal open={open} onClose={handleClose}>
-                <Modal.Header>
-                    <Modal.Title>Contact</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    E-Mail Anbindung einbauen!!!
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={handleClose} appearance="subtle">
-                        Cancel
-                    </Button>
-                    <Button onClick={handleClose} appearance="primary">
-                        Send
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+
+            <Drawer open={open} onClose={() => setOpen(false)}>
+                <Drawer.Header>
+                    <Drawer.Title>{currentlyOpen === "Imprint" ? "Imprint" : "Contact"}</Drawer.Title>
+                </Drawer.Header>
+                {currentlyOpen === "Imprint" ? <Drawer.Body>
+                    <p>Anbieter:<br />Max Mustermann<br />Musterstraße 1<br />80999 München</p>
+                    <p>Kontakt:<br />Telefon: 089/1234567-8<br />Telefax: 089/1234567-9<br />E-Mail: mail@mustermann.de<br />Website: www.mustermann.de</p>
+                    <p> </p>
+                    <p>Bei redaktionellen Inhalten:</p>
+                    <p>Verantwortlich nach § 55 Abs.2 RStV<br />Moritz Schreiberling<br />Musterstraße 2<br />80999 München</p>
+                </Drawer.Body> : <Drawer.Body>
+                    <ContactFormular />
+                </Drawer.Body>}
+            </Drawer>
         </div>
     )
 }
